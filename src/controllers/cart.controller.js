b21b3d9e8e0f5e480bf54c31ddf76266be51d7d4
NicolasAndreolli <kaiserjager10@gmail.com>
2023-10-19 15:59:31 -0300

    Update cart.controller.js

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -3,296 +3,170 @@ import CartRepository from "../repositories/cart.repository.js";
 const carro = new CartRepository();
 
 const getCarts = async (req, res) => {
-    try {
-        const result = await carro.getCarts();
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            //res.status(200).send(result);
-            res.render("carts", result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
+  try {
+    const result = await carro.getCarts();
+    if (result.error) {
+      res.status(400).send(result);
+    } else {
+      res.render("carts", result);
     }
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 const getCartById = async (req, res) => {
-    const cid = req.params.cid;
-    try {
-        const result = await carro.getCartById(cid);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            // res.status(201).send(result);
-            res.render("cart", result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
+  const cid = req.params.cid;
+  try {
+    const result = await carro.getCartById(cid);
+    if (result.error) {
+      res.status(400).send(result);
+    } else {
+      // res.status(201).send(result);
+      res.render("cart", result);
     }
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 const addCart = async (req, res) => {
-    try {
-        const result = await carro.addCart();
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
+  try {
+    const result = await carro.addCart(req.user.id);
+    if (result.error) {
+      res.status(400).send(result);
+    } else {
+      res.status(201).send(result);
     }
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 const addProduct = async (req, res) => {
-    const newCartProduct = {
-        cid: req.params.cid,
-        pid: req.params.pid,
-    };
-    try {
-        const result = await carro.addProduct(newCartProduct);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
+  const newCartProduct = {
+    cid: req.params.cid,
+    pid: req.params.pid,
+  };
+  try {
+    if (newCartProduct.cid === "null" && req.user.cart === null) {
+      const newCart = await carro.addCart(req.user._id);
+      console.log(newCart);
+    } else {
+      const result = await carro.addProduct(newCartProduct);
+      const updatedcart = await carro.getCartById(newCartProduct.cid);
+      if ("error" in result) {
+        updatedcart.message = {
+          type: "Error",
+          title: "El producto no pudo agregarse al carro.",
+          status: 404,
+        };
+      } else {
+        updatedcart.message = {
+          type: "success",
+          title: "Agregaste un producto a tu carro.",
+          status: 200,
+        };
+      }
+      res.status(updatedcart.message.status).render("cart", updatedcart);
     }
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 const deleteAllProducts = async (req, res) => {
-    const cid = req.params.cid;
-    try {
-        const result = await carro.deleteAllProducts(cid);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
+  const cid = req.params.cid;
+  try {
+    const result = await carro.deleteAllProducts(cid);
+    if (result.error) {
+      res.status(400).send(result);
+    } else {
+      res.status(201).send(result);
     }
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 const deleteProduct = async (req, res) => {
-    const deleteCartProduct = {
-        cid: req.params.cid,
-        pid: req.params.pid,
-    };
-    try {
-        const result = await carro.deleteProduct(deleteCartProduct);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
+  const deleteCartProduct = {
+    cid: req.params.cid,
+    pid: req.params.pid,
+  };
+  try {
+    const result = await carro.deleteProduct(deleteCartProduct);
+    if (result.error) {
+      res.status(400).send(result);
+    } else {
+      res.status(201).send(result);
     }
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 const updateProductQty = async (req, res) => {
-    const updateProduct = {
-        cid: req.params.cid,
-        pid: req.params.pid,
-        qty: req.body.qty,
-    };
-    try {
-        const result = await carro.updateProductQty(updateProduct);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
+  const updateProduct = {
+    cid: req.params.cid,
+    pid: req.params.pid,
+    qty: req.body.qty,
+  };
+  try {
+    const result = await carro.updateProductQty(updateProduct);
+    if (result.error) {
+      res.status(400).send(result);
+    } else {
+      res.status(201).send(result);
     }
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 const updateAllProducts = async (req, res) => {
-    const cid = req.params.cid;
-    const products = req.body;
-    try {
-        const result = await carro.updateAllProducts(cid, products);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
+  const cid = req.params.cid;
+  const products = req.body;
+  try {
+    const result = await carro.updateAllProducts(cid, products);
+    if (result.error) {
+      res.status(400).send(result);
+    } else {
+      res.status(201).send(result);
     }
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 const closeCart = async (req, res) => {
-    const cart = {
-        cid: req.params.cid,
-        user: req.user,
-    };
-    try {
-        const result = await carro.closeCart(cart);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
+  const cart = {
+    cid: req.params.cid,
+    user: req.user,
+  };
+  try {
+    const result = await carro.closeCart(cart);
+    if ("error" in result) {
+      result.message = {
+        type: "Error",
+        title: "Error al generar el ticket",
+        text: "No pudimos completar la compra vuelve a imtentarlo mas tarde.",
+        status: 404,
+      };
+    } else {
+      result.message = {
+        type: "success",
+        title: "Compra finalizada",
+        text: "Recibirás un email con los detalles de la compra",
+        status: 200,
+      };
     }
+    res.status(result.message.status).render("cartpurchase", result);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 
-/*
-
-   getCarts = async (limit = 10, page = 1, query = "{}", sort) => {
-        return this.cartDao.getCarts(limit, page, query, sort);
-    };
-    getCartById = async (cid) => {
-        return this.cartDao.getCartById(cid);
-    };
-    addCart = async () => {
-        return this.cartDao.addCart();
-    };
-    addProduct = async ({ cid, pid }) => {
-        return this.cartDao.addProduct({ cid, pid });
-    };
-    updateAllProducts = async (cid, products) => {
-        return this.cartDao.updateAllProducts(cid, products);
-    };
-    updateProductQty = async ({ cid, pid, qty }) => {
-        return this.cartDao.updateProductQty({ cid, pid, qty });
-    };
-    deleteAllProducts = async (cid) => {
-        return this.cartDao.deleteAllProducts(cid);
-    };
-    deleteProduct = async ({ cid, pid }) => {
-        return this.cartDao.deleteProduct({ cid, pid });
-    };
-
-
-import { Router } from "express";
-import { CartManagerDB } from "../dao/CartManagerDB.js";
-
-const router = Router();
-const carro = new CartManagerDB();
-
-router.get("/", async (req, res) => {
-    try {
-        const result = await carro.getCarts();
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.render("carts", result)
-        }
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-router.get("/:cid", async (req, res) => {
-    const cid = req.params.cid;
-    try {
-        const result = await carro.getCartById(cid);
-        if (result.error) {
-           res.status(400).send(result);
-        } else {
-            // res.status(201).send(result);
-            res.render("cart", result)
-        }
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-router.post("/", async (req, res) => {
-    try {
-        const result = await carro.addCart();
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-router.post("/:cid/product/:pid", async (req, res) => {
-    const newCartProduct = {
-        cid: req.params.cid,
-        pid: req.params.pid,
-    };
-    try {
-        const result = await carro.addProduct(newCartProduct);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-router.delete("/:cid", async (req, res) => {
-    const cid = req.params.cid;
-    try {
-        const result = await carro.deleteAllProducts(cid);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-router.delete("/:cid/product/:pid", async (req, res) => {
-    const deleteCartProduct = {
-        cid: req.params.cid,
-        pid: req.params.pid,
-    };
-    try {
-        const result = await carro.deleteProduct(deleteCartProduct);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-router.put("/:cid/product/:pid", async (req, res) => {
-    const updateProduct = {
-        cid: req.params.cid,
-        pid: req.params.pid,
-        qty: req.body.qty,
-    };
-    try {
-        const result = await carro.updateProductQty(updateProduct);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-router.put("/:cid", async (req, res) => {
-    const cid = req.params.cid;
-    const products = req.body;
-    try {
-        const result = await carro.updateAllProducts(cid, products);
-        if (result.error) {
-            res.status(400).send(result);
-        } else {
-            res.status(201).send(result);
-        }
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-export default router;
-
-*/
 export default {
-    getCarts,
-    getCartById,
-    addCart,
-    addProduct,
-    deleteAllProducts,
-    deleteProduct,
-    updateProductQty,
-    updateAllProducts,
-    closeCart,
+  getCarts,
+  getCartById,
+  addCart,
+  addProduct,
+  deleteAllProducts,
+  deleteProduct,
+  updateProductQty,
+  updateAllProducts,
+  closeCart,
 };
